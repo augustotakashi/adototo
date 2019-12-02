b47ce85a2b789f29b03c725de682eb3cec923569
HaulZhao <haul_zhao@hotmail.com>
2019-12-02 20:52:15 -0300

    mais coisas

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,14 +1,13 @@
-class Pet < ApplicationRecord
+ class Pet < ApplicationRecord
 	before_destroy :not_referenced_by_any_line_item
 	belongs_to :user, optional: true
 	has_many :line_items
 	
 	mount_uploader :image, ImageUploader
-	serialize :image, JSON #If yo use SQLITE, add this line
+	serialize :image, JSON 
 
 	validates :name, :age, :size, :sex, :location, presence: true
 	validates :description, length: {maximum: 1000, too_long: " Somente %{count} caracteres permitidos :("}
-	#validates :title, length: {maximum: 50, too_long: " Somente %{count} caracteres permitidos :("}
 
 	ANIMAL = %w{ Cachorro Gato }
 	SIZE = %w{ P M G }
