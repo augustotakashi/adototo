b51e2fa6e7ff5d075f2c4f9f961afd9a7a5b1eeb
HaulZhao <haul_zhao@hotmail.com>
2019-12-03 02:08:30 -0300

    quase pronto

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -58,7 +58,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id === session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url}
+      format.html { redirect_to root_path}
       format.json { head :no_content }
     end
   end
