5813604a75fbc888e3dc697bea4a847ab069946f
HaulZhao <haul_zhao@hotmail.com>
2019-12-04 14:20:10 -0300

    14.20

--- db/migrate/20191111185133_add_quantity_to_line_items.rb
+++ db/migrate/20191111185133_add_quantity_to_line_items.rb
@@ -1,5 +1,5 @@
 class AddQuantityToLineItems < ActiveRecord::Migration[6.0]
   def change
-  	add_column :line_items, :quantity, :integer, default: 1
+  	add_column :line_items default: 1
   end
 end
